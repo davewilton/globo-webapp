1e8d85c615a276fc98b752e9e5c7f04e14ec2ec3
David Wilton <david.wilton@ngis.com.au>
2025-04-02 01:39:07 +0000

    validate issue

--- resources.tf
+++ resources.tf
@@ -56,8 +56,8 @@ resource "aws_instance" "main" {
   user_data = templatefile("./templates/userdata.sh", {
     playbook_repository = var.playbook_repository
     secret_id           = var.api_key_secret_id
-    host_list_ssm_name  = aws_ssm_parameter.host_list.name
-    site_name_ssm_name  = aws_ssm_parameter.site_name.name
+    host_list_ssm_name  = local.host_list_ssm_name
+    site_name_ssm_name  = local.site_name_ssm_name
   })
 
 }
